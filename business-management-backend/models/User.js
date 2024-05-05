d22e3a088fb5b418391e90b33d863e5a4ba85afa
mohammadashrafulislam <ixdell41@gmail.com>
2024-05-05 19:56:22 +0600

    userModel created

---
+++ business-management-backend/models/User.js
@@ -0,0 +1,38 @@
+import { ObjectId } from "mongodb";
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String, 
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String, 
+        required: true,
+    },
+    avatar:{type: String},
+    role:{
+        type:String, 
+        required: true,
+        enum:['manager', 'owner', 'employee']
+    },
+    businessProfile:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'business',
+    },
+    created_at:{
+        type:Date,
+        default:Date.now
+    },
+    updated_at:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+module.exports = mongoose.model('users', UserSchema)
