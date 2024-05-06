ae8bad375d1f25f0d7f3b5cb01296f760287e1eb
mohammadashrafulislam <ixdell41@gmail.com>
2024-05-06 07:05:20 +0600

    update user route

--- business-management-backend/routes/user.js
+++ business-management-backend/routes/user.js
@@ -1,12 +1,16 @@
 import express from "express";
-import { addUser, getUser, getUsersWithBusiness } from "../controllers/userController.js";
+import { addUser, getUser, getUsersWithBusiness, updateUser } from "../controllers/userController.js";
 
 export const userRouter = express.Router();
 const router = express.Router();
 // Post API for user
 userRouter.post('/', addUser)
-// GET API for users (employee/employer) associated with business.
-router.get('/business/:businessId', getUsersWithBusiness)
-
 // get user with unique Id:
 userRouter.get('/:id', getUser)
+// update user with id route
+userRouter.put('/:id', updateUser)
+
+
+
+// GET API for users (employee/employer) associated with business.
+router.get('/business/:businessId', getUsersWithBusiness)
