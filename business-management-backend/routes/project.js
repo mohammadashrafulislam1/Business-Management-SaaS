0f8dd7f4046221a3fcdcd7b5e742479c3f1989c7
mohammadashrafulislam <ixdell41@gmail.com>
2024-05-14 16:01:21 +0600

    update project is working

--- business-management-backend/routes/project.js
+++ business-management-backend/routes/project.js
@@ -1,9 +1,7 @@
 import express from "express";
-import { addProject, getPorjectsWithBusinessId,getSpecificProject } from "../controllers/projectController.js";
+import { addProject, getPorjectsWithBusinessId,getSpecificProject, updateProject } from "../controllers/projectController.js";
 
 export const projectRouter = express.Router();
-
-
 
 // post Project
 projectRouter.post('/', addProject)
@@ -13,3 +11,6 @@ projectRouter.get('/business/:businessId', getPorjectsWithBusinessId)
 
 // get specific project with id:
 projectRouter.get('/:id', getSpecificProject)
+
+// update project route:
+projectRouter.put('/:id', updateProject)
